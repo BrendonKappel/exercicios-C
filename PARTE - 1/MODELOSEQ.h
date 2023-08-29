6045a10a0e0507ee84cc8965211bedb4103a5170
Brendon Kappel <BrendonKappel@gmail.com>
2023-08-29 18:26:38 -0300

    parte 1 do trabalho

---
+++ PARTE - 1/MODELOSEQ.h
@@ -0,0 +1,11 @@
+
+#define N_MAX   100
+
+typedef struct{
+       char nome[2];
+}INFORMACAO;
+
+typedef struct{
+       INFORMACAO info[N_MAX];  // dados do registro
+       int f;                   // número de registro(s) ocupado(s) na estrutura
+}NODO;                          // estrutura do tipo NODO
